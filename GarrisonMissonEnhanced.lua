7c89b678ebdb7a4849146f1be7a7a44385f973ca
tekkub :bear:  <tekkub@gmail.com>
2014-11-29 05:56:04 -0700

    Clean up the counters code

--- GarrisonMissonEnhanced.lua
+++ GarrisonMissonEnhanced.lua
@@ -1,65 +1,34 @@
 
 local myname, ns = ...
 
-local f = CreateFrame("frame")
-local counters = {}
-
-
-function f:CreateCounter(missionid)
-	counters = {}
-	local mission_counter = C_Garrison.GetBuffedFollowersForMission(missionid)
-	for k,v in pairs(mission_counter) do
-		for _,v2 in pairs(v) do
-			if not counters[v2["name"]] then
-				counters[v2["name"]] = {}
-			end
-			if(C_Garrison.GetFollowerStatus(k)==GARRISON_FOLLOWER_ON_MISSION or C_Garrison.GetFollowerStatus(k)==GARRISON_FOLLOWER_INACTIVE) then
-				counters[v2["name"]][k] = true
-			else
-				counters[v2["name"]][k] = false
-			end
-		end
-	end
-end
-
-local function checkemptyavail(tabl)
-	for _,v in pairs(tabl) do
-		if v == false then
-			return true
-		end
-	end
-end
-
 
 ns.inactive_statii = {
 	[GARRISON_FOLLOWER_ON_MISSION] = true,
 	[GARRISON_FOLLOWER_INACTIVE] = true,
 }
 local function GetCounterText(trait, missionid)
-	if not counters[trait] or not next(counters[trait]) then
-		return "|cff9d9d9d--"
-	else
-		local available, total = 0, 0
+	local available, total = 0, 0
 
-		local buffed = C_Garrison.GetBuffedFollowersForMission(missionid)
-		for guid,buffs in pairs(buffed) do
-			for i,buff in pairs(buffs) do
-				if buff.name == trait then
-					total = total + 1
+	local buffed = C_Garrison.GetBuffedFollowersForMission(missionid)
+	for guid,buffs in pairs(buffed) do
+		for i,buff in pairs(buffs) do
+			if buff.name == trait then
+				total = total + 1
 
-					local status = C_Garrison.GetFollowerStatus(guid)
-					if not ns.inactive_statii[status] then
-						available = available + 1
-					end
+				local status = C_Garrison.GetFollowerStatus(guid)
+				if not ns.inactive_statii[status] then
+					available = available + 1
 				end
 			end
 		end
+	end
 
-		if available == 0 then
-			return "|cff9d9d9d".. available.. "/".. total
-		else
-			return available.. "/".. total
-		end
+	if total == 0 then
+		return "|cff9d9d9d--"
+	elseif available == 0 then
+		return "|cff9d9d9d".. available.. "/".. total
+	else
+		return available.. "/".. total
 	end
 end
 
@@ -75,8 +44,6 @@ local function UpdateMission(frame, mission)
 	frame.extraEnhancedText:Show()
 	local extratext = mission.numFollowers.. " followers"
 	frame.extraEnhancedText:SetText(extratext)
-
-	f:CreateCounter(missionID)
 
 	local _, _, _, _, _, _, _, missionbosses = C_Garrison.GetMissionInfo(missionID)
 	local anchor = frame.Rewards[mission.numRewards]
