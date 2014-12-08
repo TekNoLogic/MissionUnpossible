003253aeef3699c3bf049122c76f5499ae2905b8
tekkub :bear:  <tekkub@gmail.com>
2014-12-08 00:29:06 -0700

    Don't auto-add high levels to lower level quests

--- AutoAdd.lua
+++ AutoAdd.lua
@@ -4,18 +4,20 @@ local myname, ns = ...
 
 local counters = setmetatable({}, {__index = function(t,i) return 0 end})
 local buffed
-local function GetFollowerWithBuff(mechanic)
+local function GetFollowerWithBuff(mechanic, level)
 	local follower
 	local counter = counters[mechanic]
 	counters[mechanic] = counters[mechanic] - 1
 
 	for guid,buffs in pairs(buffed) do
-		for i,buff in pairs(buffs) do
-			if buff.name == mechanic then
-				if ns.IsFollowerAvailable(guid, true) then
-					if counter == 0 then return end
-					counter = counter - 1
-					follower = guid
+		if level == 100 or C_Garrison.GetFollowerLevel(guid) == level then
+			for i,buff in pairs(buffs) do
+				if buff.name == mechanic then
+					if ns.IsFollowerAvailable(guid, true) then
+						if counter == 0 then return end
+						counter = counter - 1
+						follower = guid
+					end
 				end
 			end
 		end
@@ -39,7 +41,7 @@ hooksecurefunc("GarrisonMissionPage_ShowMission", function(mission)
 
 	for _,boss in pairs(missionbosses) do
 		for _,mechanic in pairs(boss.mechanics) do
-			local guid = GetFollowerWithBuff(mechanic.name)
+			local guid = GetFollowerWithBuff(mechanic.name, mission.level)
 			if guid then GarrisonMissionPage_AddFollower(guid) end
 		end
 	end
