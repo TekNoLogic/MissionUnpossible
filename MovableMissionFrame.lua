12496060e4c02f20206ee9fd09d98e1d5bc88b69
Kevin Ballard <kevin@sb.org>
2014-12-13 01:52:09 -0800

    Allow dragging the garrison mission UI frame

---
+++ MovableMissionFrame.lua
@@ -0,0 +1,9 @@
+
+local myname, ns = ...
+
+
+local frame = GarrisonMissionFrame
+frame:SetMovable(true)
+frame:RegisterForDrag("LeftButton")
+frame:SetScript("OnDragStart", frame.StartMoving)
+frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
