a657b4d917c98bbd8641fbc0aa1ec24197e1c98e
tekkub :bear:  <tekkub@gmail.com>
2015-01-03 21:02:18 -0700

    Update externals

--- externals/debug.lua
+++ externals/debug.lua
@@ -2,13 +2,5 @@
 local myname, ns = ...
 
 
-local name, _, _, enabled = GetAddOnInfo("tekDebug")
-local player = UnitName("player")
-local enabled = GetAddOnEnableState(player, "tekDebug") == 2
-if enabled and not IsAddOnLoaded("tekDebug") then
-	local succ, err = LoadAddOn(name)
-end
-
-
 local debugf = tekDebug and tekDebug:GetFrame(myname)
 function ns.Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
