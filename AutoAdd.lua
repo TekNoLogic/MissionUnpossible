9b233437318fe39683bd844ab7bf1273920925ec
tekkub :bear:  <tekkub@gmail.com>
2015-02-01 19:04:37 -0700

    Auto-add leveling followers on <lv100 missions
    If followers are still needed after matching abilities, and it's not a
    lv100 mission, fill in toons that are high enough to do the mission but
    not yet lv100 themselves.

--- AutoAdd.lua
+++ AutoAdd.lua
@@ -37,6 +37,23 @@ local function GetFollowerWithBuff(mechanic, level)
 end
 
 
+local function EligibleFollower(follower, mission_level)
+	if not follower.isCollected then return false end
+	if follower.status then return false end
+	return follower.level < 100 and follower.level >= mission_level
+end
+
+
+local followers
+local function NumPotentialFollowers(mission_level)
+	local count = 0
+	for _,follower in pairs(followers) do
+		if EligibleFollower(follower, mission_level) then count = count + 1 end
+	end
+	return count
+end
+
+
 local dirty = false
 hooksecurefunc("GarrisonMissionPage_ShowMission", function() dirty = true end)
 hooksecurefunc("GarrisonFollowerList_UpdateFollowers", function()
@@ -47,6 +64,7 @@ hooksecurefunc("GarrisonFollowerList_UpdateFollowers", function()
 
 	wipe(counters)
 
+	local needed = mission.numFollowers
 	local _, _, _, _, _, _, _, missionbosses = C_Garrison.GetMissionInfo(mission.missionID)
 	buffed = C_Garrison.GetBuffedFollowersForMission(mission.missionID)
 
@@ -59,8 +77,21 @@ hooksecurefunc("GarrisonFollowerList_UpdateFollowers", function()
 	for _,boss in pairs(missionbosses) do
 		for _,mechanic in pairs(boss.mechanics) do
 			local guid = GetFollowerWithBuff(mechanic.name, mission.level)
-			if guid then GarrisonMissionPage_AddFollower(guid) end
+			if guid then
+				needed = needed - 1
+				GarrisonMissionPage_AddFollower(guid)
+			end
 		end
 	end
 
+	if needed == 0 or mission.level == 100 then return end
+
+	followers = C_Garrison.GetFollowers()
+	if NumPotentialFollowers(mission.level) > needed then return end
+
+	for _,follower in pairs(followers) do
+		if EligibleFollower(follower, mission.level) then
+			GarrisonMissionPage_AddFollower(follower.followerID)
+		end
+	end
 end)
