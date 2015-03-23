ea955d6d138d40cb630466d6ea0861dcdbf92113
tekkub :bear:  <tekkub@gmail.com>
2015-03-22 18:41:12 -0600

    Add race and env bonuses

--- AbilitySummary.lua
+++ AbilitySummary.lua
@@ -12,7 +12,7 @@ local icon = butt:CreateTexture(nil, "BORDER")
 icon:SetAllPoints()
 icon:SetTexture("Interface\\Icons\\garrison_building_barracks")
 
-local tip = ns.NewTooltip(10)
+local tip = ns.NewTooltip(12)
 
 
 
@@ -34,6 +34,30 @@ local function CanCounter(follower, mechanic)
       if mechanicID == mechanic then return tex end
     end
   end
+end
+
+
+local missiondetails
+local GetUncounteredMechanics = C_Garrison.GetMissionUncounteredMechanics
+local function IsShown(follower, mission)
+  if not IsMaxLevel(follower) then return false end
+  if mission.iLevel < 645 then return true end
+
+  if not missiondetails then
+    local mechs = GetUncounteredMechanics(mission.missionID)
+    missiondetails = {}
+    for _,mechIDs in pairs(mechs) do
+      for _,mechID in pairs(mechIDs) do
+        missiondetails[mechID] = true
+      end
+    end
+  end
+
+  for id in pairs(missiondetails) do
+    if CanCounter(follower, id) then return true end
+  end
+
+  return false
 end
 
 
@@ -62,6 +86,40 @@ local function LastAbility(follower)
 end
 
 
+local RACIALS = {}
+for i=63,75 do RACIALS[i] = true end -- Core races
+for i=252,255 do RACIALS[i] = true end -- Other races
+local function RacialText(follower)
+  local str = ""
+  for i=1,6 do
+    local traitID = C_Garrison.GetFollowerTraitAtIndex(follower.followerID, i)
+    if RACIALS[traitID] then
+      local icon = C_Garrison.GetFollowerAbilityIcon(traitID)
+      if icon then str = str.. "|T".. icon.. ":16|t" end
+    end
+  end
+  return str
+end
+
+
+local FIVEPERC = {[201] = true} -- Combat experience
+for i=36,43 do FIVEPERC[i] = true end -- Slayers
+for i=44,49 do FIVEPERC[i] = true end -- Environs
+for i=7,9 do FIVEPERC[i] = true end
+for i=76,77 do FIVEPERC[i] = true end
+local function BonusText(follower)
+  local str = ""
+  for i=1,6 do
+    local traitID = C_Garrison.GetFollowerTraitAtIndex(follower.followerID, i)
+    if FIVEPERC[traitID] then
+      local icon = C_Garrison.GetFollowerAbilityIcon(traitID)
+      if icon then str = str.. "|T".. icon.. ":16|t" end
+    end
+  end
+  return str
+end
+
+
 local function sorter(a,b)
   local fa = FirstAbility(a)
   local fb = FirstAbility(b)
@@ -74,6 +132,9 @@ end
 
 butt:SetScript("OnLeave", function() tip:Hide() end)
 butt:SetScript("OnEnter", function(self)
+  local mission = GarrisonMissionFrame.MissionTab.MissionPage.missionInfo
+  missiondetails = nil
+
   local followers = C_Garrison.GetFollowers()
   table.sort(followers, sorter)
 
@@ -86,7 +147,7 @@ butt:SetScript("OnEnter", function(self)
   tip:AddLine(" ")
 
   for i,follower in pairs(followers) do
-    if IsMaxLevel(follower) then
+    if IsShown(follower, mission) then
       local wag = CounterText(follower, 1)  -- Wild Aggression
       local mst = CounterText(follower, 2)  -- Massive Strike
       local gda = CounterText(follower, 3)  -- Group Damage
@@ -97,8 +158,12 @@ butt:SetScript("OnEnter", function(self)
       local dmi = CounterText(follower, 9)  -- Deadly Minions
       local tba = CounterText(follower, 10) -- Timed Battle
 
+      local racial = RacialText(follower)
+      local bonus = BonusText(follower)
+
       tip:AddMultiLine(follower.name,
                        wag, mst, gda, mde, dzo, msw, psp, dmi, tba,
+                       racial, bonus,
                        1,1,1)
     end
   end
