1f7f18533213dc59bc96033462e7097400b2aa7c
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-07-25 01:29:40 -0600

    Fix follower right click

--- FollowerClick.lua
+++ FollowerClick.lua
@@ -6,7 +6,7 @@ local MissionPage = GarrisonMissionFrame.MissionTab.MissionPage
 local function RemoveFollower(followerID)
 	for i,frame in pairs(MissionPage.Followers) do
 		if frame.info and frame.info.followerID == followerID then
-			return GarrisonMissionPage_ClearFollower(frame, true)
+			return GarrisonMissionFrame:RemoveFollowerFromMission(frame, true)
 		end
 	end
 end
@@ -16,7 +16,7 @@ local orig = GarrisonFollowerListButton_OnClick
 function GarrisonFollowerListButton_OnClick(self, button, ...)
 	if MissionPage:IsVisible() and MissionPage.missionInfo and button == "RightButton" then
 		if not self.info.status then
-			return GarrisonMissionPage_AddFollower(self.id)
+			return MissionPage:AddFollower(self.id)
 		elseif self.info.status == GARRISON_FOLLOWER_IN_PARTY then
 			return RemoveFollower(self.id)
 		end
