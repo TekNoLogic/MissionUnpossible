5d48e126f4b8b4c90c05e8ef8a2ee41418720c16
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-08-02 03:50:28 -0600

    Repair follower TNL

--- FollowerTNL.lua
+++ FollowerTNL.lua
@@ -2,11 +2,11 @@
 local myname, ns = ...
 
 
-hooksecurefunc("GarrisonFollowerList_Update", function(self)
-	local followers = self.FollowerList.followers
-	local followersList = self.FollowerList.followersList
+hooksecurefunc(GarrisonMissionFrame.FollowerList, "UpdateData", function(self)
+	local followers = self.followers
+	local followersList = self.followersList
 
-	local scrollFrame = self.FollowerList.listScroll
+	local scrollFrame = self.listScroll
 	local offset = HybridScrollFrame_GetOffset(scrollFrame)
 
 	for i,button in pairs(scrollFrame.buttons) do
@@ -14,12 +14,12 @@ hooksecurefunc("GarrisonFollowerList_Update", function(self)
 		if index <= #followersList then
 			local follower = followers[followersList[index]]
 			if ns.IsFollowerLeveling(follower) and not follower.status then
-				button.Name:SetPoint("LEFT", button.PortraitFrame, "LEFT", 66, 8)
+				button.Follower.Name:SetPoint("LEFT", button.Follower.PortraitFrame, "LEFT", 66, 8)
 
 				local tnl = follower.levelXP - follower.xp
-				button.Status:SetText(GARRISON_FOLLOWER_TOOLTIP_XP:format(tnl))
+				button.Follower.Status:SetText(GARRISON_FOLLOWER_TOOLTIP_XP:format(tnl))
 
-				button.XPBar:SetTexture(94/256, 50/256, 119/256)
+				button.Follower.XPBar:SetTexture(94/256, 50/256, 119/256)
 			end
 		end
 	end
