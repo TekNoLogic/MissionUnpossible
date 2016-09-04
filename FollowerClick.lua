d99582afb117545fd20daeb0f9bce2688a72c3d0
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-09-03 18:05:41 -0600

    Add class hall support to FollowerClick module

--- FollowerClick.lua
+++ FollowerClick.lua
@@ -2,25 +2,36 @@
 local myname, ns = ...
 
 
-local MissionPage = GarrisonMissionFrame.MissionTab.MissionPage
-local function RemoveFollower(followerID)
-	for i,frame in pairs(MissionPage.Followers) do
-		if frame.info and frame.info.followerID == followerID then
-			return GarrisonMissionFrame:RemoveFollowerFromMission(frame, true)
+local function Hook(missionpage)
+	local function RemoveFollower(followerID)
+		for i,frame in pairs(missionpage.Followers) do
+			if frame.info and frame.info.followerID == followerID then
+				return GarrisonMissionFrame:RemoveFollowerFromMission(frame, true)
+			end
 		end
+	end
+
+
+	local orig = GarrisonFollowerListButton_OnClick
+	function GarrisonFollowerListButton_OnClick(self, button, ...)
+		if missionpage:IsVisible() and missionpage.missionInfo and button == "RightButton" then
+			if not self.info.status then
+				return missionpage:AddFollower(self.id)
+			elseif self.info.status == GARRISON_FOLLOWER_IN_PARTY then
+				return RemoveFollower(self.id)
+			end
+		end
+
+		return orig(self, button, ...)
 	end
 end
 
 
-local orig = GarrisonFollowerListButton_OnClick
-function GarrisonFollowerListButton_OnClick(self, button, ...)
-	if MissionPage:IsVisible() and MissionPage.missionInfo and button == "RightButton" then
-		if not self.info.status then
-			return MissionPage:AddFollower(self.id)
-		elseif self.info.status == GARRISON_FOLLOWER_IN_PARTY then
-			return RemoveFollower(self.id)
-		end
-	end
+function ns.InitGarrison.FollowerClick()
+	Hook(GarrisonMissionFrame.MissionTab.MissionPage)
+end
 
-	return orig(self, button, ...)
+
+function ns.InitOrderHall.FollowerClick()
+	Hook(OrderHallMissionFrame.MissionTab.MissionPage)
 end
